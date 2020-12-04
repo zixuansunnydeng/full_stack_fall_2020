93e91d586fc7f722c57bf3625910eaf407362920
Sheldon Chi <chi.sheldon@gmail.com>
2020-12-03 22:39:14 -0800

    default / to twitter

--- backend/django_intro/mysite/mysite/urls.py
+++ backend/django_intro/mysite/mysite/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('twitter/', include('twitter.urls')),
+    path('', include('twitter.urls')),
     path('blog/', include('blog.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
 ]
